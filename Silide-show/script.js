20071b297663d7b4a980db08418cb43b53c87f36
KhanhVan2212 <vannkph22621@fpt.edu.vn>
2024-10-02 21:08:13 +0700

    Add Slide-show

---
+++ Silide-show/script.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slides");
+let dots = document.getElementsByClassName("dot");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active-dot";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
+
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+}
+
+showSlides();  // Initial call
